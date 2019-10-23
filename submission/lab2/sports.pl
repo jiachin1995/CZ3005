4789c44e091714784137ed35b256cceb7f69d920
Ching Jia Chin <jiachin1995@gmail.com>
2019-10-23 22:34:48 +0800

    added game mode

--- submission/lab2/sports.pl
+++ submission/lab2/sports.pl
@@ -4,30 +4,31 @@ sports([tennis, diving, golf, baseball, soccer, rugby, basketball, ultimate_fris
 
 
 /* Game contents */
-tennis([court, outdoor, singles, doubles, ball, racket, scores, maxteamsize(2)]).
-diving([pool, indoor, singles, scores, maxteamsize(1)]).
-golf([field, outdoor, singles, outdoor, scores, ball, club, maxteamsize(1)]).
-baseball([field, outdoor, outdoor, ball, bat, scores, maxteamsize(9) ]).
-soccer([field, outdoor, ball, scores, maxteamsize(11) ]).
-rugby([field, outdoor, ball, scores, maxteamsize(15)]).
-basketball([court, indoor, ball, scores, maxteamsize(5)]).
-ultimate_frisbee([field, outdoor, scores, frisbee, maxteamsize(7)]).
-volleyball([court, indoor, ball, scores, maxteamsize(6)]).
-badminton([court, indoor, singles, doubles, scores, shuttlecock, racket, maxteamsize(2)]).
-swimming([pool, indoor, fastest, maxteamsize(1)]).
-skiing([field, outdoor,skiis, fastest, maxteamsize(1)]).
-surfing([sea, outdoor,surfboard, scores, maxteamsize(1)]).
-archery([field, court, indoor, bow_arrow, scores, maxteamsize(1)]).
-karate([court, indoor, scores, maxteamsize(1)]).
-bowling([court, indoor, ball, scores, maxteamsize(5)]).
-dodgeball([court, indoor, ball, scores, maxteamsize(6)]).
-table_tennis([court, singles, doubles, indoor, scores, ball, racket, maxteamsize(3)]). 
-hockey([court, indoor, puck, hockeystick, scores, maxteamsize(6)]).
+tennis([court, outdoor, singles, doubles, ball, racket, scores, knockout, maxteamsize(2)]).
+diving([pool, indoor, singles, scores, performance, maxteamsize(1)]).
+golf([field, outdoor, singles, outdoor, scores, ball, performance, club, maxteamsize(1)]).
+baseball([field, outdoor, outdoor, ball, bat, scores, performance, maxteamsize(9) ]).
+soccer([field, outdoor, ball, scores, timed, maxteamsize(11) ]).
+rugby([field, outdoor, ball, scores, timed, maxteamsize(15)]).
+basketball([court, indoor, ball, scores, timed, maxteamsize(5)]).
+ultimate_frisbee([field, outdoor, scores, frisbee, timed, maxteamsize(7)]).
+volleyball([court, indoor, ball, scores, knockout, maxteamsize(6)]).
+badminton([court, indoor, singles, doubles, scores, knockout, shuttlecock, racket, maxteamsize(2)]).
+swimming([pool, indoor, fastest, performance, maxteamsize(1)]).
+skiing([field, outdoor,skiis, fastest, performance, performance, maxteamsize(1)]).
+surfing([sea, outdoor,surfboard, scores, performance, maxteamsize(1)]).
+archery([field, court, indoor, bow_arrow, scores, performance, maxteamsize(1)]).
+karate([court, indoor, scores, knockout, maxteamsize(1)]).
+bowling([court, indoor, ball, scores, performance, maxteamsize(5)]).
+dodgeball([court, indoor, ball, scores, knockout, maxteamsize(6)]).
+table_tennis([court, singles, doubles, indoor, scores, knockout, ball, racket, maxteamsize(3)]). 
+hockey([court, indoor, puck, hockeystick, scores, timed, maxteamsize(6)]).
 
 /*list of options. Options are items that contains a list as their arity. Used by help.pl. */
-options([equipment, performancetype]).
+options([equipment, scoringtype, gamemode]).
 
 
 /*options and their lists*/
 equipment([ball, puck, racket,frisbee,surfboard,skiis, bow_arrow, shuttlecock, bat, club, hockeystick]).
-performancetype([scores, fastest]).
+scoringtype([scores, fastest]).
+gamemode([knockout,timed, performance])
